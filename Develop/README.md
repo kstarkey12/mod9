21aba25b33ed9ac017e21062da3ba590da8521f7
kstarkey12 <ktarkey47@gmail.com>
2023-07-04 00:40:19 -0400

    Ive lost track

---
+++ Develop/README.md
@@ -0,0 +1,35 @@
+
+# pro. read me gen
+
+## Description
+a project where I utilize node to generate readmes for me in the terminal
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+undefined
+
+## Usage
+undefined
+
+## License
+This application is covered under the undefined license. [![License: undefined](https://img.shields.io/badge/License-undefined-blue.svg)](https://opensource.org/licenses/undefined)
+
+## Contributing
+undefined
+
+## Tests
+undefined
+
+## Questions
+If you have any questions, feel free to reach out to me via GitHub or email.
+
+GitHub: [undefined](https://github.com/undefined)
+
+Email: undefined
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,70 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Prompt the user for information
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter your project title:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description:',
+    },
+    // Add more prompts for installation, usage, contribution, tests, license, GitHub username, and email
+    // ...
+  ])
+  .then((answers) => {
+    // Generate the README content based on the user's input
+    const readmeContent = `
+# ${answers.title}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+## Description
+${answers.description}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
-// Function call to initialize app
-init();
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This application is covered under the ${answers.license} license. [![License: ${answers.license}](https://img.shields.io/badge/License-${answers.license}-blue.svg)](https://opensource.org/licenses/${answers.license})
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+If you have any questions, feel free to reach out to me via GitHub or email.
+
+GitHub: [${answers.username}](https://github.com/${answers.username})
+
+Email: ${answers.email}
+`;
+
+    // Write the README content to a file
+    fs.writeFile('README.md', readmeContent, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('README.md generated successfully!');
+      }
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
